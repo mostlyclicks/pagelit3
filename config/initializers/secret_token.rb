5925b52a687f2b722062d5004f08e5412cdc23c7
mostlyclicks <carlos@mostlyclicks.com>
2012-03-28 13:01:39 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pagelit3::Application.config.secret_token = '6204217afea2228ab8d905234c521321bf1268f643d358e5625f934df9b1eff1367a938065db251b83ae0d31ec2a7e5d31d70a7b8ff5465ed039f273fc95c584'
